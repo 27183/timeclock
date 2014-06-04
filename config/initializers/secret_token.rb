de9044dcba1747bf458cae6fa5fbbb2b70e8bd16
Ryan McLean <mclean_ryan@gsb.stanford.edu>
2014-06-04 15:46:27 -0700

    added config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Timeclock::Application.config.secret_key_base = '4f9275c810ef7bc2fcedaa208c6d6fca0ae8fc5474b909267b13597bd0c03939454c65604c5f0d6fac3bbcd9bdc821a2a19dd21db451b869871473d8dd52a839'
