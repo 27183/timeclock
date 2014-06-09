a10a8b51a3ed1aa8471cb60b775d0825452c7b8a
Ryan McLean <mclean_ryan@gsb.stanford.edu>
2014-06-08 23:11:30 -0700

    A few more changes to back into 3.2 rails.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Timeclock::Application.config.secret_key_base = '7b8aa06ab72aca80ff80dd2c99f41397d0bd70053f7f5591cb17b23cdf908ee4bb81fdcf71435bdd4bb140951e063d4e00f78f747256b291ac79f0afab2f51f3'
+Timeclock::Application.config.secret_token = '7b8aa06ab72aca80ff80dd2c99f41397d0bd70053f7f5591cb17b23cdf908ee4bb81fdcf71435bdd4bb140951e063d4e00f78f747256b291ac79f0afab2f51f3'
