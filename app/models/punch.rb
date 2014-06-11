598d70c2ac68efb04203ec616cab3674c89e41ae
Ryan McLean <mclean_ryan@gsb.stanford.edu>
2014-06-11 10:30:57 -0700

    Clean..scorteched:w

--- app/models/punch.rb
+++
@@ -1,3 +0,0 @@
-class Punch < ActiveRecord::Base
-   #attr_accessible :time_in, :time_out
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Timeclock::Application.config.secret_token = '7b8aa06ab72aca80ff80dd2c99f41397d0bd70053f7f5591cb17b23cdf908ee4bb81fdcf71435bdd4bb140951e063d4e00f78f747256b291ac79f0afab2f51f3'
